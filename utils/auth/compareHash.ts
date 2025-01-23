3bc95a3d0f58390bd66379c418b9a0e64c1ca200
Adam Salama <adam.salama@cashflowpositif.fr>
2025-01-23 11:19:29 +0100

    fix part2

--- utils/auth/compareHash.ts
+++ utils/auth/compareHash.ts
@@ -1,9 +1,7 @@
 import crypto from "crypto-js";
 
-const compareHash = (userPassword: string, storedHash: string) => {
+export default function compareHash (userPassword: string, storedHash: string) {
   const userHash = crypto.SHA256(userPassword).toString();
 
   return userHash === storedHash;
 };
-
-export default compareHash;
